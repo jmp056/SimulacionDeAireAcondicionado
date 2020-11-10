49088be8fca8085ebf2feb7919b243b34cefdefe
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-11-09 22:14:13 -0400

    Automaticacion del aire al 50%

--- SimulacionDeAireAcondicionado/Control.Designer.cs
+++ SimulacionDeAireAcondicionado/Control.Designer.cs
@@ -28,19 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.TempDeseanaNumericUpDown = new System.Windows.Forms.NumericUpDown();
-            this.TempActualumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.TempDeseadaNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.TempAmbienteNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.ControlGroupBox = new System.Windows.Forms.GroupBox();
+            this.TempActualLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.EncenderButton = new System.Windows.Forms.Button();
             this.VentanaButton = new System.Windows.Forms.Button();
             this.MensajeGroupBox = new System.Windows.Forms.GroupBox();
             this.ReposoButton = new System.Windows.Forms.Button();
             this.MensajeLabel = new System.Windows.Forms.Label();
             this.PuertaButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.TempDeseanaNumericUpDown)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TempActualumericUpDown)).BeginInit();
+            this.DisminuirTimer = new System.Windows.Forms.Timer(this.components);
+            this.AumentarTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.TempDeseadaNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TempAmbienteNumericUpDown)).BeginInit();
             this.ControlGroupBox.SuspendLayout();
             this.MensajeGroupBox.SuspendLayout();
             this.SuspendLayout();
@@ -59,75 +66,122 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(30, 20);
+            this.label2.Location = new System.Drawing.Point(0, 20);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(166, 16);
+            this.label2.Size = new System.Drawing.Size(214, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Temperatura actual °C:";
+            this.label2.Text = "Temperatura del ambiente °C:";
             // 
-            // TempDeseanaNumericUpDown
+            // TempDeseadaNumericUpDown
             // 
-            this.TempDeseanaNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TempDeseanaNumericUpDown.Location = new System.Drawing.Point(220, 55);
-            this.TempDeseanaNumericUpDown.Maximum = new decimal(new int[] {
+            this.TempDeseadaNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TempDeseadaNumericUpDown.Location = new System.Drawing.Point(220, 55);
+            this.TempDeseadaNumericUpDown.Maximum = new decimal(new int[] {
             45,
             0,
             0,
             0});
-            this.TempDeseanaNumericUpDown.Minimum = new decimal(new int[] {
+            this.TempDeseadaNumericUpDown.Minimum = new decimal(new int[] {
             16,
             0,
             0,
             0});
-            this.TempDeseanaNumericUpDown.Name = "TempDeseanaNumericUpDown";
-            this.TempDeseanaNumericUpDown.Size = new System.Drawing.Size(82, 22);
-            this.TempDeseanaNumericUpDown.TabIndex = 2;
-            this.TempDeseanaNumericUpDown.Value = new decimal(new int[] {
+            this.TempDeseadaNumericUpDown.Name = "TempDeseadaNumericUpDown";
+            this.TempDeseadaNumericUpDown.Size = new System.Drawing.Size(82, 22);
+            this.TempDeseadaNumericUpDown.TabIndex = 2;
+            this.TempDeseadaNumericUpDown.Value = new decimal(new int[] {
             16,
             0,
             0,
             0});
             // 
-            // TempActualumericUpDown
+            // TempAmbienteNumericUpDown
             // 
-            this.TempActualumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TempActualumericUpDown.Location = new System.Drawing.Point(220, 15);
-            this.TempActualumericUpDown.Maximum = new decimal(new int[] {
+            this.TempAmbienteNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TempAmbienteNumericUpDown.Location = new System.Drawing.Point(220, 15);
+            this.TempAmbienteNumericUpDown.Maximum = new decimal(new int[] {
             70,
             0,
             0,
             0});
-            this.TempActualumericUpDown.Minimum = new decimal(new int[] {
+            this.TempAmbienteNumericUpDown.Minimum = new decimal(new int[] {
             16,
             0,
             0,
             0});
-            this.TempActualumericUpDown.Name = "TempActualumericUpDown";
-            this.TempActualumericUpDown.Size = new System.Drawing.Size(82, 22);
-            this.TempActualumericUpDown.TabIndex = 3;
-            this.TempActualumericUpDown.Value = new decimal(new int[] {
+            this.TempAmbienteNumericUpDown.Name = "TempAmbienteNumericUpDown";
+            this.TempAmbienteNumericUpDown.Size = new System.Drawing.Size(82, 22);
+            this.TempAmbienteNumericUpDown.TabIndex = 3;
+            this.TempAmbienteNumericUpDown.Value = new decimal(new int[] {
             16,
             0,
             0,
             0});
+            this.TempAmbienteNumericUpDown.ValueChanged += new System.EventHandler(this.TempAmbienteNumericUpDown_ValueChanged);
+            this.TempAmbienteNumericUpDown.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TempAmbienteNumericUpDown_KeyPress);
             // 
             // ControlGroupBox
             // 
+            this.ControlGroupBox.Controls.Add(this.TempActualLabel);
+            this.ControlGroupBox.Controls.Add(this.label5);
+            this.ControlGroupBox.Controls.Add(this.label4);
+            this.ControlGroupBox.Controls.Add(this.label3);
             this.ControlGroupBox.Controls.Add(this.EncenderButton);
             this.ControlGroupBox.Controls.Add(this.label2);
-            this.ControlGroupBox.Controls.Add(this.TempActualumericUpDown);
+            this.ControlGroupBox.Controls.Add(this.TempAmbienteNumericUpDown);
             this.ControlGroupBox.Controls.Add(this.label1);
-            this.ControlGroupBox.Controls.Add(this.TempDeseanaNumericUpDown);
+            this.ControlGroupBox.Controls.Add(this.TempDeseadaNumericUpDown);
             this.ControlGroupBox.Location = new System.Drawing.Point(12, 12);
             this.ControlGroupBox.Name = "ControlGroupBox";
             this.ControlGroupBox.Size = new System.Drawing.Size(344, 164);
             this.ControlGroupBox.TabIndex = 4;
             this.ControlGroupBox.TabStop = false;
             // 
+            // TempActualLabel
+            // 
+            this.TempActualLabel.AutoSize = true;
+            this.TempActualLabel.BackColor = System.Drawing.Color.Transparent;
+            this.TempActualLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TempActualLabel.Location = new System.Drawing.Point(248, 111);
+            this.TempActualLabel.Name = "TempActualLabel";
+            this.TempActualLabel.Size = new System.Drawing.Size(54, 31);
+            this.TempActualLabel.TabIndex = 7;
+            this.TempActualLabel.Text = " 16";
+            this.TempActualLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(286, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 31);
+            this.label5.TabIndex = 8;
+            this.label5.Text = " °";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(153, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Temperatura\r\n           Actual";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
             // EncenderButton
             // 
             this.EncenderButton.Image = global::SimulacionDeAireAcondicionado.Properties.Resources.Apagado;
-            this.EncenderButton.Location = new System.Drawing.Point(131, 96);
+            this.EncenderButton.Location = new System.Drawing.Point(33, 97);
             this.EncenderButton.Name = "EncenderButton";
             this.EncenderButton.Size = new System.Drawing.Size(85, 50);
             this.EncenderButton.TabIndex = 4;
@@ -197,6 +251,16 @@
             this.PuertaButton.UseVisualStyleBackColor = true;
             this.PuertaButton.Click += new System.EventHandler(this.PuertaButton_Click);
             // 
+            // DisminuirTimer
+            // 
+            this.DisminuirTimer.Interval = 3000;
+            this.DisminuirTimer.Tick += new System.EventHandler(this.DisminuirTimer_Tick);
+            // 
+            // AumentarTimer
+            // 
+            this.AumentarTimer.Interval = 6000;
+            this.AumentarTimer.Tick += new System.EventHandler(this.AumentarTimer_Tick);
+            // 
             // Control
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -209,8 +273,8 @@
             this.Name = "Control";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Control de aire acondicionado";
-            ((System.ComponentModel.ISupportInitialize)(this.TempDeseanaNumericUpDown)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.TempActualumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TempDeseadaNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TempAmbienteNumericUpDown)).EndInit();
             this.ControlGroupBox.ResumeLayout(false);
             this.ControlGroupBox.PerformLayout();
             this.MensajeGroupBox.ResumeLayout(false);
@@ -223,8 +287,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.NumericUpDown TempDeseanaNumericUpDown;
-        private System.Windows.Forms.NumericUpDown TempActualumericUpDown;
+        private System.Windows.Forms.NumericUpDown TempDeseadaNumericUpDown;
+        private System.Windows.Forms.NumericUpDown TempAmbienteNumericUpDown;
         private System.Windows.Forms.GroupBox ControlGroupBox;
         private System.Windows.Forms.Button EncenderButton;
         private System.Windows.Forms.Button PuertaButton;
@@ -232,6 +296,12 @@
         private System.Windows.Forms.GroupBox MensajeGroupBox;
         private System.Windows.Forms.Label MensajeLabel;
         private System.Windows.Forms.Button ReposoButton;
+        private System.Windows.Forms.Label TempActualLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer DisminuirTimer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer AumentarTimer;
     }
 }
 
--- SimulacionDeAireAcondicionado/Control.cs
+++ SimulacionDeAireAcondicionado/Control.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SimulacionDeAireAcondicionado
@@ -18,95 +11,81 @@ namespace SimulacionDeAireAcondicionado
         }
 
         bool ventana = false, puerta = false, aire = false, reposo = false;
+        
         decimal t = 0, tr = 30;
 
-        bool validar()
-        {
-            bool paso = true;
+        //bool validar()
+        //{
+        //    bool paso = true;
 
-            if (ventana == true && puerta == true)
-            {
-                paso = false;
-                MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la puerta y la ventana";
-            }
-            else if (puerta == true)
-            {
-                paso = false;
-                MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la puerta";
-            }
-            else if (ventana == true)
-            {
-                paso = false;
-                MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la ventana";
-            }
+        //    if (ventana == true && puerta == true)
+        //    {
+        //        paso = false;
+        //        MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la puerta y la ventana";
+        //    }
+        //    else if (puerta == true)
+        //    {
+        //        paso = false;
+        //        MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la puerta";
+        //    }
+        //    else if (ventana == true)
+        //    {
+        //        paso = false;
+        //        MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la ventana";
+        //    }
 
-            decimal ta = TempActualumericUpDown.Value, td = TempDeseanaNumericUpDown.Value;
-            if (td > ta)
-                MensajeLabel.Text = "La temperatura actual es menor a la deseada";
+        //    decimal ta = TempAmbienteNumericUpDown.Value, td = TempDeseadaNumericUpDown.Value;
+        //    if (td > ta)
+        //        MensajeLabel.Text = "La temperatura actual es menor a la deseada";
 
-            return paso;
-        }
+        //    return paso;
+        //}
 
         void Calcular()
         {
-            if(reposo == false)
-            {
-                if(!validar())
-                    return;
+            //if(reposo == false)
+            //{
 
-                decimal ta = TempActualumericUpDown.Value, td = TempDeseanaNumericUpDown.Value;
-                t = (ta - td) * 2;
-                MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nllevar el cuarto a {1} grados °C", t, td);
+            //    decimal ta = TempAmbienteNumericUpDown.Value, td = TempDeseadaNumericUpDown.Value;
+            //    t = (ta - td) * 2;
+            //    MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nllevar el cuarto a {1} grados °C", t, td);
 
-                ReposoButton.Visible = true;
-            }
+            //    ReposoButton.Visible = true;
+            //}
 
-            if (reposo == true)
-            {
-                decimal tra = 0;
+            //if (reposo == true)
+            //{
+            //    decimal tra = 0;
 
-                if(puerta == true && ventana == true)
-                {
-                    tra = tr - (tr * Convert.ToDecimal(0.35));
-                    MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nreposo", tra);
-                }
-                else if (puerta == true)
-                {
-                    tra = 30 - (tr * Convert.ToDecimal(0.25));
-                    MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nreposo", tra);
-                }
-                else if (ventana == true)
-                {
-                    tra = 30 - (tr * Convert.ToDecimal(0.10));
-                    MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nreposo", tra);
-                }
-                else
-                    MensajeLabel.Text = "El aire aconticionado tardara 30 minutos en \nreposo";
-            }
+            //    if(puerta == true && ventana == true)
+            //    {
+            //        tra = tr - (tr * Convert.ToDecimal(0.35));
+            //        MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nreposo", tra);
+            //    }
+            //    else if (puerta == true)
+            //    {
+            //        tra = 30 - (tr * Convert.ToDecimal(0.25));
+            //        MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nreposo", tra);
+            //    }
+            //    else if (ventana == true)
+            //    {
+            //        tra = 30 - (tr * Convert.ToDecimal(0.10));
+            //        MensajeLabel.Text = string.Format("El aire aconticionado tardara {0} minutos en \nreposo", tra);
+            //    }
+            //    else
+            //        MensajeLabel.Text = "El aire aconticionado tardara 30 minutos en \nreposo";
+            //}
         }
 
 
 
-        private void EncenderButton_Click(object sender, EventArgs e)
-        {
-            if(aire == false)
-            {
-                aire = true;
-                EncenderButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Encendido;
-                Calcular();
-            }
-            else
-            {
-                aire = false;
-                MensajeLabel.Text = string.Empty;
-                EncenderButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Apagado;
-                ReposoButton.Visible = false;
-            }
-        }
+        
+
+
 
         private void PuertaButton_Click(object sender, EventArgs e)
         {
-            if(puerta == false)
+            if (puerta == false)
             {
                 puerta = true;
                 PuertaButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.PuertaAbiertaImg;
@@ -122,9 +101,12 @@ namespace SimulacionDeAireAcondicionado
                 PuertaButton.Text = "Abrir puerta";
             }
 
-            if (aire == true)
-                Calcular();
+            //if (aire == true)
+            //    Calcular();
         }
+
+
+
         private void VentanaButton_Click(object sender, EventArgs e)
         {
             if (ventana == false)
@@ -143,24 +125,153 @@ namespace SimulacionDeAireAcondicionado
                 VentanaButton.Text = "Abrir ventana";
             }
 
-            if (aire == true)
-                Calcular();
+            //if (aire == true)
+            //    Calcular();
         }
         private void ReposoButton_Click(object sender, EventArgs e)
         {
-            if (reposo == false)
-            {
-                reposo = true;
-                Calcular();
-                ReposoButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Play;
-            }
-            else
-            {
-                reposo = false;
-                Calcular();
-                ReposoButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Pausa;
-            }
+            //if (reposo == false)
+            //{
+            //    reposo = true;
+            //    Calcular();
+            //    ReposoButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Play;
+            //}
+            //else
+            //{
+            //    reposo = false;
+            //    Calcular();
+            //    ReposoButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Pausa;
+            //}
         }
 
+
+
+
+        //----------------------------------------------------------------------------------------------------
+        // -------
+        //--------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+        private void TempAmbienteNumericUpDown_ValueChanged(object sender, EventArgs e) 
+        { //Este evento actualiza la temperatura del label de la temperatura actual cuando se cambia por las flechas el valor del NumericUpDown de la temperatura del ambiente
+            TempActualLabel.Text = Convert.ToString(TempAmbienteNumericUpDown.Value);
+        }
+        
+        private void TempAmbienteNumericUpDown_KeyPress(object sender, KeyPressEventArgs e)
+        {//Este evento actualiza el valor del label de temperatura actual al precionar una tecla en el numericUodown de la temperatura del ambiente
+            TempActualLabel.Text = Convert.ToString(TempAmbienteNumericUpDown.Value);
+        }
+        
+        private void DisminuirTimer_Tick(object sender, EventArgs e) // Timer encargado de disminuir la temperatura
+        {
+            if (Convert.ToInt32(TempActualLabel.Text) <= TempDeseadaNumericUpDown.Value)
+            {
+                DisminuirTimer.Enabled = false;
+                Reposo();
+            }
+            else
+                if (ventana == true && puerta == true)
+                {
+                    MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la puerta y la ventana";
+                    
+                if(Convert.ToInt32(TempActualLabel.Text) < TempAmbienteNumericUpDown.Value) { 
+                        TempActualLabel.Text = Convert.ToString(Convert.ToSingle(TempActualLabel.Text) + 2);
+
+                        if (Convert.ToInt32(TempActualLabel.Text) > TempAmbienteNumericUpDown.Value)
+                            TempActualLabel.Text = Convert.ToString(TempAmbienteNumericUpDown.Value);
+                    }
+                }
+
+                else if (puerta == true)
+                {
+                    MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la puerta";
+                    
+                    if (Convert.ToInt32(TempActualLabel.Text) < TempAmbienteNumericUpDown.Value)
+                        TempActualLabel.Text = Convert.ToString(Convert.ToSingle(TempActualLabel.Text) + 1);
+                }
+                else if (ventana == true)
+                {
+                    MensajeLabel.Text = "El cuarto no alcanzara la temperatura deseada \ndebe cerrar la ventana";
+                    
+                    if (Convert.ToInt32(TempActualLabel.Text) < TempAmbienteNumericUpDown.Value)
+                        TempActualLabel.Text = Convert.ToString(Convert.ToSingle(TempActualLabel.Text) + 1);
+                }
+
+                else
+                {
+                    MensajeLabel.Text = "¡El aire acondicionado se encendido!";
+                    TempActualLabel.Text = Convert.ToString(Convert.ToSingle(TempActualLabel.Text) - 1);
+                }
+                    
+        }
+
+        public void validar()
+        {
+            //if(ventana == true)
+
+        }
+
+        public void Reposo() // Funcion que trabaja cuando el aire se encuentra en reposo
+        {
+            MensajeLabel.Text = "¡El aire acondicionado se encuentra en reposo!";
+            AumentarTimer.Enabled = true;
+        }
+        
+        private void AumentarTimer_Tick(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(TempActualLabel.Text) >= TempDeseadaNumericUpDown.Value + 3)
+            {
+                AumentarTimer.Enabled = false;
+                Encender();
+            }
+            else
+                TempActualLabel.Text = Convert.ToString(Convert.ToSingle(TempActualLabel.Text) + 1);
+        }
+        
+        private void EncenderButton_Click(object sender, EventArgs e)
+        { //Clic al boton encender
+            if(aire == false)
+            {
+
+                aire = true;
+                EncenderButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Encendido;
+                TempAmbienteNumericUpDown.Enabled = false;
+                Encender();
+            }
+
+
+            //if(aire == false)
+            //{
+            //    DisminuirTimer.Interval = 3000;
+            //    DisminuirTimer.Enabled = true;
+
+            //    aire = true;
+            //    
+            //    
+
+            //    Calcular();
+            //}
+            //else
+            //{
+            //    aire = false;
+            //    TempAmbienteNumericUpDown.Enabled = true;
+            //    MensajeLabel.Text = string.Empty;
+            //    EncenderButton.Image = SimulacionDeAireAcondicionado.Properties.Resources.Apagado;
+            //    ReposoButton.Visible = false;
+            //}
+        }
+
+
+        public void Encender()
+        {
+            MensajeLabel.Text = "¡El aire acondicionado se encendido!";
+            DisminuirTimer.Enabled = true;
+        }
     }
 }
